68cdb1a347f41804aab6e1bbfbe1453a7d665063
Pablo Rivero <pblrvo@gmail.com>
2024-06-19 23:33:13 +0200

    modified data preparation to implement kfold cross validation

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -5,102 +5,80 @@ from hyperparameter_tuning import MAX_SEQ_LENGTH
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
+# Load features
 videos_df = load_features('./resources/JSON/')
 
 
-#Split data into train & test dataframes while ensuring label representation
-def split_data_with_label_representation(df):
+# Split data into keypoints and labels
+def split_data_and_labels(df):
     labels = df["label"].to_numpy()
+    keypoints_df = df.drop(columns=["label"])
 
-  # For label-aware splitting
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.15, random_state=42)
+    return keypoints_df, labels
 
-  # Splitting based on label groups
-    for train_index, test_index in sss.split(df, labels):
-        train_X = df.iloc[train_index, :-1]  
-        train_y = labels[train_index]
-        test_X = df.iloc[test_index, :-1]
-        test_y = labels[test_index]
-
-
-    return train_X, test_X, train_y, test_y
-
+# Extend keypoints to MAX_SEQ_LENGTH
 def extend_keypoints(df):
     def extend_list(keypoints):
         current_length = len(keypoints)
-        
+
         if current_length < MAX_SEQ_LENGTH:
             repeat_count = (MAX_SEQ_LENGTH - current_length) // current_length
             remainder = (MAX_SEQ_LENGTH - current_length) % current_length
-            
+
             # Repeat the keypoints to match the repeat_count
             extended_keypoints = np.tile(keypoints, (repeat_count + 1, 1))
             # Add the remaining frames
             extended_keypoints = np.vstack((extended_keypoints, keypoints[:remainder]))
-            
+
             # Truncate to MAX_SEQ_LENGTH if it's overflown
             return extended_keypoints[:MAX_SEQ_LENGTH]
         elif current_length > MAX_SEQ_LENGTH:
             # Truncate the keypoints to MAX_SEQ_LENGTH
             return keypoints[:MAX_SEQ_LENGTH]
-        
+
         return keypoints
 
     df['keypoints'] = df['keypoints'].apply(lambda x: extend_list(np.array(x)))
     return df
 
+# Standardize keypoints
 def standardize_keypoints(df):
-  scaler = StandardScaler()
-  all_standardized_keypoints = []
-  df = df.reset_index(drop=True)
-  for i in range(len(df)):
-    keypoints = df.loc[i, 'keypoints']
+    scaler = StandardScaler()
+    all_standardized_keypoints = []
+    df = df.reset_index(drop=True)
+    for i in range(len(df)):
+        keypoints = df.loc[i, 'keypoints']
 
-    standardized_video_keypoints = []
-    for frame_keypoints in keypoints:
-      # Reshape to 2D for StandardScaler
-      frame_keypoints = frame_keypoints.reshape(-1, 1)  # Reshape to (n_samples, n_features)
-      # Standardize using StandardScaler
-      standardized_frame_keypoints = scaler.fit_transform(frame_keypoints)
-      # Reshape back to original frame format
-      standardized_frame_keypoints = standardized_frame_keypoints.reshape(-1)
-      standardized_video_keypoints.append(standardized_frame_keypoints)
+        standardized_video_keypoints = []
+        for frame_keypoints in keypoints:
+            # Reshape to 2D for StandardScaler
+            frame_keypoints = frame_keypoints.reshape(-1, 1)  # Reshape to (n_samples, n_features)
+            # Standardize using StandardScaler
+            standardized_frame_keypoints = scaler.fit_transform(frame_keypoints)
+            # Reshape back to original frame format
+            standardized_frame_keypoints = standardized_frame_keypoints.reshape(-1)
+            standardized_video_keypoints.append(standardized_frame_keypoints)
 
-    all_standardized_keypoints.append(standardized_video_keypoints)
+        all_standardized_keypoints.append(standardized_video_keypoints)
 
-  return all_standardized_keypoints  # Return the list of standardized video keypoints
+    return all_standardized_keypoints  # Return the list of standardized video keypoints
 
-
-
-
-
+# Build tensors from dataframe
 def build_tensors(df):
-    
-  train_X, test_X, train_y, test_y = split_data_with_label_representation(df)
+    df_data, df_labels = split_data_and_labels(df)
 
-  train_data_extended = extend_keypoints(train_X)
-  test_data_extended = extend_keypoints(test_X)
+    df_data = extend_keypoints(df_data)
+    data_extended = standardize_keypoints(df_data)
 
-  train_data_extended = standardize_keypoints(train_data_extended)
-  test_data_extended = standardize_keypoints(test_data_extended)
+    data_tensors = []
+    for video in data_extended:
+        # Iterate through frames in the video
+        frame_tensors = []
+        for frame in video:
+            # Convert each frame into a tensor
+            frame_tensor = tf.convert_to_tensor(frame, dtype=tf.float32)
+            frame_tensors.append(frame_tensor)
+        # Append the list of frame tensors (representing the video) to the final list
+        data_tensors.append(frame_tensors)
 
-  train_X_tensor = []
-  for video in train_data_extended:
-    # Iterate through frames in the video
-    frame_tensors = []
-    for frame in video:
-      # Convert each frame into a tensor
-      frame_tensor = tf.convert_to_tensor(frame, dtype=tf.float32)
-      frame_tensors.append(frame_tensor)
-    # Append the list of frame tensors (representing the video) to the final list
-    train_X_tensor.append(frame_tensors)
-
-  test_X_tensor = []
-  for video in test_data_extended:
-    frame_tensors = []
-    for frame in video:
-      frame_tensor = tf.convert_to_tensor(frame, dtype=tf.float32)
-      frame_tensors.append(frame_tensor)
-    test_X_tensor.append(frame_tensors)
-
-  return train_X_tensor, train_y, test_X_tensor, test_y
+    return np.array(data_tensors), df_labels
--- src/model_compilation.py
+++ src/model_compilation.py
@@ -7,11 +7,10 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.preprocessing import LabelEncoder
 import keras_tuner as kt
 from hyperparameter_tuning import MyHyperModel, EPOCHS
+from sklearn.model_selection import KFold
 
 videos_df = data_collection.load_features('./resources/JSON/')
-train_data, train_labels, test_data, test_labels = build_tensors(videos_df)
-train_data = np.array(train_data)
-test_data = np.array(test_data)
+X, y = build_tensors(videos_df)
 
 def run_experiment():
     filepath = "/tmp/video_classifier_model.keras"
@@ -21,33 +20,91 @@ def run_experiment():
     early_stopping = EarlyStopping(monitor='val_accuracy', patience=10)
     
     le = LabelEncoder()
-    train_labels_encoded = le.fit_transform(train_labels)
-    test_labels_encoded = le.transform(test_labels)
+    y_encoded = le.fit_transform(y)
 
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    fold_no = 1
+    val_accuracies = []
+
+    for train_index, val_index in kf.split(X):
+        print(f"Training fold {fold_no}...")
+        X_train, X_val = X[train_index], X[val_index]
+        y_train, y_val = y_encoded[train_index], y_encoded[val_index]
+
+        print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
+        print(f"X_val shape: {X_val.shape}, y_val shape: {y_val.shape}")
+
+        print(f"Starting hyperparameter tuning for fold {fold_no}...")
+        tuner = kt.RandomSearch(
+            MyHyperModel(),
+            objective='val_accuracy',
+            max_trials=10,  # Reduce trials for testing
+            executions_per_trial=1,
+            directory='my_dir',
+            project_name=f'hyperparam_tuning_fold_{fold_no}',
+            overwrite=True,
+        )
+
+        tuner.search(X_train, y_train, epochs=EPOCHS, validation_data=(X_val, y_val), batch_size=8, callbacks=[checkpoint, early_stopping])
+        print(f"Hyperparameter tuning completed for fold {fold_no}")
+
+        print(f"Retrieving the best model for fold {fold_no}...")
+        best_model = tuner.get_best_models(num_models=1)[0]
+
+        print(f"Starting training of the best model for fold {fold_no}...")
+        history = best_model.fit(
+            X_train, y_train,
+            validation_data=(X_val, y_val),
+            epochs=EPOCHS,
+            batch_size=8,
+            callbacks=[early_stopping],
+            verbose=1
+        )
+        print(f"Training completed for fold {fold_no}")
+
+        val_accuracy = max(history.history['val_accuracy'])
+        val_accuracies.append(val_accuracy)
+        print(f"Fold {fold_no} validation accuracy: {val_accuracy:.4f}")
+        
+        fold_no += 1
+
+    print(f"Average validation accuracy over {kf.n_splits} folds: {np.mean(val_accuracies):.4f}")
+
+    print("Starting final training on all data...")
     tuner = kt.RandomSearch(
         MyHyperModel(),
         objective='val_accuracy',
-        max_trials=10,
+        max_trials=3,  # Reduce trials for testing
         executions_per_trial=1,
         directory='my_dir',
-        project_name='hyperparam_tuning',
+        project_name='hyperparam_tuning_final',
         overwrite=True,
     )
 
-    tuner.search(train_data, train_labels_encoded, epochs=EPOCHS, validation_split=0.15, batch_size=8, callbacks=[checkpoint, early_stopping])
+    tuner.search(X, y_encoded, epochs=EPOCHS, validation_split=0.15, batch_size=8, callbacks=[checkpoint, early_stopping])
+    print("Hyperparameter tuning completed for final model")
+
+    print("Retrieving the best model for final training...")
     best_model = tuner.get_best_models(num_models=1)[0]
+
+    print("Starting training of the best final model...")
     history = best_model.fit(
-        train_data, train_labels_encoded,
+        X, y_encoded,
         validation_split=0.15,
         epochs=EPOCHS,
         batch_size=8,
+        callbacks=[early_stopping],
+        verbose=1
     )
+    print("Training of the best final model completed")
+
     plot_training_history(history)
     best_model.save(filepath)
+    print(f"Model saved to {filepath}")
     
     best_model = keras.models.load_model(filepath)
     
-    _, accuracy = best_model.evaluate(test_data, test_labels_encoded)
+    _, accuracy = best_model.evaluate(X, y_encoded)
     print(f"Test accuracy: {round(accuracy * 100, 2)}%")
 
     return best_model
